8d8679957048c115826e7cd39288a84cf792e652
bhavneet2910 <bhavneet2910pal@gmail.com>
2025-04-02 22:26:12 +0530

    updated io.hpp

---
+++ src/app/fileHandling/IO.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include "IO.hpp"
+#include <fstream>
+
+IO::IO(const std::string& file_path)
+{
+    file_stream.open(file_path, std::ios::in | std::ios::out | std::ios::binary);
+    if (!file_stream.is_open()) {
+        std::cout << "Unable to open file: " << file_path << std::endl;
+    }
+}
+
+IO::~IO() {
+    if (file_stream.is_open()) {
+        file_stream.close();
+    }
+}
+
+
+
+
+
+std::fstream IO::getFileStream() {
+    return std::move(file_stream);
+}
